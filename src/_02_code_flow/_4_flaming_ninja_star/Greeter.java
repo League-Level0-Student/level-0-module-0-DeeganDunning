a0b03c641ab3b388da292f0b04c975fede034540
student <student@192.168.1.54>
2023-08-24 14:02:03 -0700

    Madlibs in progress

---
+++ src/_02_code_flow/_4_flaming_ninja_star/Greeter.java
@@ -0,0 +1,12 @@
+package _02_code_flow._4_flaming_ninja_star;
+
+import javax.swing.JOptionPane;
+
+public class Greeter { 
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	String input = JOptionPane.showInputDialog("What is your name?");
+	JOptionPane.showMessageDialog(null,"Hello "+input);
+	}
+	
+}
